73aa310872444c6cb7597d87a639fd6f405a8924
Evgeny Nazarov <e.v.nazarov@mail.ru>
2020-07-10 10:02:46 +1000

    Temporary site refresh

---
+++ precache-manifest.3b8bda374d50d578e0e7f9aa0250aca1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6ea07bcbcb11dbb1f903c0882f40c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63731df94e71717b2526",
+    "url": "/static/css/main.8cb37a9d.chunk.css"
+  },
+  {
+    "revision": "a6f657ca44f553d72938",
+    "url": "/static/js/2.8679621f.chunk.js"
+  },
+  {
+    "revision": "6a1f60932d4b758cc1944d93356f455d",
+    "url": "/static/js/2.8679621f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63731df94e71717b2526",
+    "url": "/static/js/main.8901a2e3.chunk.js"
+  },
+  {
+    "revision": "497b41ccf9b239b3003d",
+    "url": "/static/js/runtime-main.8b61ca00.js"
+  },
+  {
+    "revision": "2e7f48e9ebf50ac33b731f2a5302d66b",
+    "url": "/static/media/logo.2e7f48e9.svg"
+  },
+  {
+    "revision": "a9420a6d52a7b9f5ccae570fd2e65a60",
+    "url": "/static/media/site.a9420a6d.svg"
+  }
+]);
